374826768d026f596624cd3f6d2650c5350b1e6f
kjankiewicz <jankiewicz.krzysztof@gmail.com>
2025-03-12 13:34:21 +0100

    zmiany

--- make.bat
+++ make.bat
@@ -1,8 +1,7 @@
 @echo off
 cd %~dp0
 
-docker build -t hadoop-hive-spark-base ./base
-docker build -t hadoop-hive-spark-master ./master
-docker build -t hadoop-hive-spark-worker ./worker
-docker build -t hadoop-hive-spark-history ./history
-docker build -t hadoop-hive-spark-jupyter ./jupyter
+docker build -t hadoop-spark-base ./base
+docker build -t hadoop-spark-master ./master
+docker build -t hadoop-spark-worker ./worker
+docker build -t hadoop-spark-history ./history
