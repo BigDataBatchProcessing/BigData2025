87d26354d63c8621c79d0d7c3b4837fb2df7573a
kjankiewicz <jankiewicz.krzysztof@gmail.com>
2025-03-13 09:33:41 +0100

    dodatkowy port

--- make.bat
+++ make.bat
@@ -5,3 +5,4 @@ docker build -t hadoop-spark-base ./base
 docker build -t hadoop-spark-master ./master
 docker build -t hadoop-spark-worker ./worker
 docker build -t hadoop-spark-history ./history
+docker build -t hadoop-spark-hive ./hive
