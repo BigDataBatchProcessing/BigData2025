4a0cfa103ac6b6f4e77f53fcd497be93da274311
kjankiewicz <jankiewicz.krzysztof@gmail.com>
2025-03-13 11:35:43 +0100

    testowo

--- hive/run.sh
+++ hive/run.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/bash
 
+HIVE_HOME=/opt/hive
+
 if [ ! -f "$HIVE_HOME"/initialized ]; then
-  echo "Configuring Hive..."
+  echo "Configuring Hive X..."
   hdfs dfs -mkdir -p  /user/hive/warehouse
 #  schematool -dbType postgres -initSchema
 #  touch "$HIVE_HOME"/initialized
 fi
 
-echo "Starting Hive Metastore..."
-hive --service metastore &
+# echo "Starting Hive Metastore..."
+# hive --service metastore &
 
-echo "Starting Hive server2..."
-hiveserver2 --hiveconf hive.root.logger=DEBUG,console &
+# echo "Starting Hive server2..."
+# hiveserver2 --hiveconf hive.root.logger=DEBUG,console &
+
+# Trzymamy kontener w działaniu
+echo "Waiting for services to run..."
+while true; do
+  sleep 1000
+done
